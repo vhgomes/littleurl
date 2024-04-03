a95b92e82caa47abcf59b0a55f6cadae4540d97f
Victor Hugo <vhclaudiogomes@gmail.com>
2024-04-03 15:48:44 -0300

    initial commit

---
+++ src/main/java/com/vhomes/littleurl/LittleurlApplication.java
@@ -0,0 +1,13 @@
+package com.vhomes.littleurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleurlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleurlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vhomes/littleurl/LittleurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhomes.littleurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleurlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
