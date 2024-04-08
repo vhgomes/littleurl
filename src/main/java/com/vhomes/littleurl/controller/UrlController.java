0165434e5b00b7d7e85cb0d6d2733b3a814cb316
Victor Hugo <vhclaudiogomes@gmail.com>
2024-04-08 17:25:47 -0300

    controller added

---
+++ src/main/java/com/vhomes/littleurl/controller/UrlController.java
@@ -0,0 +1,65 @@
+package com.vhomes.littleurl.controller;
+
+import com.vhomes.littleurl.DTOS.CreateShortUrlDTO;
+import com.vhomes.littleurl.models.URL;
+import com.vhomes.littleurl.repository.UrlRepository;
+import com.vhomes.littleurl.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+public class UrlController {
+    private final UserRepository userRepository;
+    private final UrlRepository urlRepository;
+
+    public UrlController(UserRepository userRepository, UrlRepository urlRepository) {
+        this.userRepository = userRepository;
+        this.urlRepository = urlRepository;
+    }
+
+    @GetMapping("/urls")
+    public ResponseEntity<Optional<URL>> getAllUrls(JwtAuthenticationToken jwtAuthenticationToken) {
+        var user = userRepository.findById(UUID.fromString(jwtAuthenticationToken.getName()));
+        var urls = urlRepository.findById(user.get().getUserId());
+        return ResponseEntity.ok(urls);
+    }
+
+    @PostMapping("/urls")
+    public ResponseEntity<URL> createShortUrl(JwtAuthenticationToken jwtAuthenticationToken, @RequestBody CreateShortUrlDTO shortUrlDTO) {
+        var user = userRepository.findById(UUID.fromString(jwtAuthenticationToken.getName())).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var url = new URL();
+        url.setOriginalUrl(shortUrlDTO.originalUrl());
+        url.setCreatedBy(user);
+        url.setClicksOnLink(0);
+        URL saved = urlRepository.save(url);
+
+        var id = saved.generateRandomShortUUID(saved.getUrl_id());
+        url.setShortUrl(id);
+
+        urlRepository.save(url);
+
+        return ResponseEntity.ok(saved);
+    }
+
+    @GetMapping("/{id}")
+    public RedirectView redirectTo(@PathVariable("id") String urlId) {
+        URL urlToRedirect = urlRepository.findByShortUrl(urlId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        urlToRedirect.upVisitedClicks();
+        urlRepository.save(urlToRedirect);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl(urlToRedirect.getOriginalUrl());
+        return redirectView;
+    }
+}
+
+
+
+
---
+++ src/main/java/com/vhomes/littleurl/controller/UserController.java
@@ -0,0 +1,86 @@
+package com.vhomes.littleurl.controller;
+
+import com.vhomes.littleurl.DTOS.CreateUserDTO;
+import com.vhomes.littleurl.DTOS.LoginRequestDTO;
+import com.vhomes.littleurl.DTOS.LoginResponseDTO;
+import com.vhomes.littleurl.models.Role;
+import com.vhomes.littleurl.models.User;
+import com.vhomes.littleurl.repository.RoleRepository;
+import com.vhomes.littleurl.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.Instant;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+
+@RestController
+public class UserController {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final JwtEncoder jwtEncoder;
+
+    public UserController(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder, JwtEncoder jwtEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    @PostMapping("/users")
+    @Transactional
+    public ResponseEntity<Void> register(@RequestBody CreateUserDTO createUserDTO) {
+        var basicRole = roleRepository.findByName(Role.Values.BASIC.name());
+        var userExists = userRepository.findByUsername(createUserDTO.username());
+
+        if (userExists.isPresent()) throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+
+        var user = new User();
+        user.setName(createUserDTO.name());
+        user.setUsername(createUserDTO.username());
+        user.setPassword(passwordEncoder.encode(createUserDTO.password()));
+        user.setRoles(Set.of(basicRole));
+
+        userRepository.save(user);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        var user = userRepository.findByUsername(loginRequestDTO.username());
+
+        if (user.isEmpty() || !user.get().isLoginCorrect(loginRequestDTO, passwordEncoder)) {
+            throw new BadCredentialsException("user or password is invalid");
+        }
+
+        var now = Instant.now();
+        var expiresIn = 300L;
+        var scopes = user.get().getRoles().stream().map(Role::getName).collect(Collectors.joining(" "));
+
+        var claims = JwtClaimsSet.builder()
+                .issuer("backend")
+                .subject(user.get().getUserId().toString())
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiresIn))
+                .claim("scope", scopes)
+                .build();
+
+        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+
+        return ResponseEntity.ok(new LoginResponseDTO(jwtValue, expiresIn));
+    }
+
+
+}
