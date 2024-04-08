6b2fbfe416a75ba99e55c85390945b8cd03822ee
Victor Hugo <vhclaudiogomes@gmail.com>
2024-04-08 17:24:59 -0300

    security added

---
+++ src/main/java/com/vhomes/littleurl/config/AdminUserConfig.java
@@ -0,0 +1,48 @@
+package com.vhomes.littleurl.config;
+
+import com.vhomes.littleurl.models.Role;
+import com.vhomes.littleurl.models.User;
+import com.vhomes.littleurl.repository.RoleRepository;
+import com.vhomes.littleurl.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Set;
+
+@Configuration
+public class AdminUserConfig implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public AdminUserConfig(RoleRepository roleRepository, UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        var roleAdmin = roleRepository.findByName(Role.Values.ADMIN.name());
+        var userAdmin = userRepository.findByUsername("admin");
+
+        userAdmin.ifPresentOrElse(
+                (user) -> {
+                    System.out.println("Admin já existe");
+                },
+                () -> {
+                    var user = new User();
+                    user.setUsername("admin");
+                    user.setPassword(bCryptPasswordEncoder.encode("123"));
+                    user.setRoles(Set.of(roleAdmin));
+                    user.setName("Victor Hugo Cláudio Gomes");
+                    userRepository.save(user);
+                }
+        );
+
+    }
+}
